819d96f6ab346da66b65dc5162c14dbc578275a3
DanilaBratinov <bratinov0704@inbox.ru>
2022-11-04 20:13:30 +0300

    Initial Commit

---
+++ HW6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW6
+//
+//  Created by Данила Братинов on 04.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
